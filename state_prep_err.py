0f91af44fe3568277f6f59c9a342cd668dde3dc7
APerezFadon <49376581+APerezFadon@users.noreply.github.com>
2022-01-22 09:02:59 +0000

    Fixed bug

--- state_prep_err.py
+++ state_prep_err.py
@@ -4,10 +4,10 @@ from scipy.linalg import sqrtm
 import matplotlib.pyplot as plt
 
 
-N = 2
+N = 1
 phi = 1
 
-eps = 1
+eps = 0.1
 
 def rho_experimental(eps, N):
 	rho = np.zeros((4**N, 2**N, 2**N), dtype = np.complex128)
@@ -34,14 +34,25 @@ def get_infidelities(A, B, N):
 		infid[i] = 1 - np.real(fidelity(A[i], B[i]))
 	return infid
 
-def single_trial(eps):
+def single_trial(eps, N):
 	rho_t = get_rho(N)
 	rho_e = rho_experimental(eps, N)
 
 	infidelities = get_infidelities(rho_t, rho_e, N)
 
 	eta_t, _ = find_eta(rho_t, phi, N)
-	eta_e, _ = find_eta(rho_e, phi, N)
+
+	# Find eta_e
+	sigma = get_sigma(N)
+	P = get_projectors(N)
+
+	U = dynamics_unitary(phi, sigma, N)
+	rho_final = rhof_theoretical(U, rho_e, N)
+
+	_, Prob_unpacked = get_probs(P, rho_final, N)
+	_, A_unpacked = get_A(P, sigma, rho_t, N)
+
+	eta_e = get_eta(A_unpacked, Prob_unpacked, N)
 
 	diff = eta_t - eta_e
 	deta = np.real(np.trace(sqrtm(diff.conj().T @ diff)))
@@ -49,13 +60,13 @@ def single_trial(eps):
 
 	return np.mean(infidelities), deta
 
-n_samples = 100
-eps_max = 1
+n_samples = 1000
+eps_max = 0.1
 infids = np.zeros((n_samples, ))
 detas = np.zeros((n_samples, ))
 for i in range(n_samples):
 	eps = i * eps_max / n_samples
-	a, b = single_trial(eps)
+	a, b = single_trial(eps, N)
 	infids[i] = a
 	detas[i] = b
 
@@ -63,7 +74,7 @@ data = np.column_stack((infids, detas))
 np.savetxt("data.csv", data)
 
 plt.title("State preparation errors")
-plt.scatter(infids, detas)
+plt.scatter(infids, detas, marker = "x")
 plt.grid()
 plt.xlabel("Infidelities")
 plt.ylabel(r"$||\eta^{T} - \eta^{E}||$")
